3985acc7a9fcacb2f880e264f0a65fb07049e757
Max <max.heinzer397@gmx.ch>
2023-02-26 17:10:19 +0100

    feat: add everforest

---
+++ lua/omega/colors/custom/everforest.lua
@@ -0,0 +1,11 @@
+local colors = require("omega.colors.themes.everforest").colors
+vim.g.terminal_color_foreground = colors.white
+
+return {
+    ["Visual"] = { bg = colors.light_grey },
+    ["CursorLine"] = { bg = colors.light_grey },
+    ["FloatBorder"] = { fg = colors.light_grey },
+    ["CmpCompletionWindowBorder"] = { fg = colors.light_grey },
+    ["CmpDocumentationWindowBorder"] = { fg = colors.light_grey },
+    ["WinSeparator"] = { fg = colors.light_grey },
+}
