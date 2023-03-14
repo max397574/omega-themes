16a5d21e0f7e550fca7c31ab0f0b8be41575c8c1
Max <max.heinzer397@gmx.ch>
2023-03-14 14:15:41 +0100

    better cmp highlight

--- lua/omega/colors/custom/ayu_light.lua
+++ lua/omega/colors/custom/ayu_light.lua
@@ -4,6 +4,8 @@ local color_utils = require("omega.utils.colors")
 return {
     highlights = {
         TSField = { fg = base16.base0E },
+        CmpNormal = { fg = colors.white, bg = color_utils.darken_color(colors.darker_black, 10) },
+        CmpBorder = { fg= color_utils.darken_color(colors.darker_black, 10) },
         IncSearch = { bg = color_utils.blend_colors(colors.orange, colors.black, 0.75) },
         ["@tag.delimiter"] = { fg = colors.base0D },
         ["@parameter"] = { fg = base16.base09 },
