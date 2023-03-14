f3701c7c91f9ae6692318d4ced02f12f89632a20
Max <max.heinzer397@gmx.ch>
2023-03-14 09:00:41 +0100

    feat(ayu_light): better color for IncSearch

--- lua/omega/colors/custom/ayu_light.lua
+++ lua/omega/colors/custom/ayu_light.lua
@@ -1,10 +1,10 @@
 local base16 = require("omega.colors.themes.ayu_light").base16
 local colors = require("omega.colors.themes.ayu_light").colors
+local color_utils = require("omega.utils.colors")
 return {
     highlights = {
         TSField = { fg = base16.base0E },
-        IncSearch = { bg = colors.orange },
-        PmenuSel = { fg = colors.white, bg = colors.pmenu_bg },
+        IncSearch = { bg = color_utils.blend_colors(colors.orange, colors.black, 0.75) },
         ["@tag.delimiter"] = { fg = colors.base0D },
         ["@parameter"] = { fg = base16.base09 },
         ["@constructor"] = { fg = base16.base0C },
